e8d5e2eada3b13db7280030f4ccb154c4d796b49
Richard de Wit <henk.exe@gmail.com>
2019-08-19 11:55:09 +0200

    Show users what install.sh is doing

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+echo "sudo curl -L https://raw.githubusercontent.com/richarddewit/sly/master/sly -o /usr/local/bin/sly"
 sudo curl -L https://raw.githubusercontent.com/richarddewit/sly/master/sly -o /usr/local/bin/sly
+echo "sudo chmod +x /usr/local/bin/sly"
 sudo chmod +x /usr/local/bin/sly
